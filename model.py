05b960dcce4a047b9fdba3474126250db0804de8
Phil Gold <phil@cs.jhu.edu>
2015-06-03 16:08:39 -0400

    Move account info into its own class.

---
+++ model.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+import pwd
+import subprocess
+
+from datetime import datetime, timedelta
+
+import iso8601  # https://bitbucket.org/micktwomey/pyiso8601
+
+def parse_datetime(field):
+    """Parses an ISO-8601-formatted datetime.  Returns None for None."""
+    if field:
+        return iso8601.parse_date(field)
+    else:
+        return None
+
+class AccountInfo:
+    def __init__(self, filesystem, uid, db_cursor):
+        self.filesystem = filesystem
+        self.uid = uid
+        self.db_cursor = db_cursor
+        try:
+            self.username = pwd.getpwuid(self.uid).pw_name
+        except KeyError:
+            self.username = '#%d' % self.uid
+
+        self.db_cursor.execute('SELECT * FROM entry WHERE filesystem = ? AND uid = ?',
+                               (self.filesystem, self.uid))
+        # RHEL 5's pysqlite is too old to have indexing by field name, so we
+        # have to fake it.
+        indices = {}
+        for i in xrange(0, len(self.db_cursor.description)):
+            indices[self.db_cursor.description[i][0]] = i
+
+        row = self.db_cursor.fetchone()
+        if row:
+            self.blocks_used = row[indices['blocks_used']]
+            self.block_quota = row[indices['block_soft_limit']]
+            self.block_limit = row[indices['block_hard_limit']]
+            self.block_grace_expires = parse_datetime(row[indices['block_grace_expires']])
+            self.inodes_used = row[indices['inodes_used']]
+            self.inode_quota = row[indices['inode_soft_limit']]
+            self.inode_limit = row[indices['inode_hard_limit']]
+            self.inode_grace_expires = parse_datetime(row[indices['inode_grace_expires']])
+            self.last_update = parse_datetime(row[indices['last_update']])
+            self.block_quota_notify = parse_datetime(row[indices['block_quota_notify']])
+            self.block_hard_notify = parse_datetime(row[indices['block_hard_notify']])
+            self.block_ok_notify = parse_datetime(row[indices['block_ok_notify']])
+            self.inode_quota_notify = parse_datetime(row[indices['inode_quota_notify']])
+            self.inode_hard_notify = parse_datetime(row[indices['inode_hard_notify']])
+            self.inode_ok_notify = parse_datetime(row[indices['inode_ok_notify']])
+        else:
+            self.blocks_used = None
+            self.block_quota = None
+            self.block_limit = None
+            self.block_grace_expires = None
+            self.inodes_used = None
+            self.inode_quota = None
+            self.inode_limit = None
+            self.inode_grace_expires = None
+            self.last_update = None
+            self.block_quota_notify = None
+            self.block_hard_notify = None
+            self.block_ok_notify = None
+            self.inode_quota_notify = None
+            self.inode_hard_notify = None
+            self.inode_ok_notify = None
+            
+    @staticmethod
+    def from_quotatool(filesystem, uid, db_cursor):
+        ai = AccountInfo(filesystem, uid, db_cursor)
+
+        qt = subprocess.Popen(['quotatool', '-u', str(uid), '-d', filesystem],
+                              stdout=subprocess.PIPE)
+        stdout, stderr = qt.communicate()
+        qt_uid, qt_fs, \
+            blocks_used, block_quota, block_limit, block_grace, \
+            inodes_used, inode_quota, inode_limit, inode_grace = \
+            stdout.split()
+
+        ai.blocks_used = int(blocks_used)
+        ai.block_quota = int(block_quota)
+        ai.block_limit = int(block_limit)
+        ai.inodes_used = int(inodes_used)
+        ai.inode_quota = int(inode_quota)
+        ai.inode_limit = int(inode_limit)
+
+        if int(block_grace) == 0:
+            ai.block_grace_expires = None
+        else:
+            ai.block_grace_expires = \
+                datetime.now().replace(microsecond=0) + timedelta(seconds=int(block_grace))
+        if int(inode_grace) == 0:
+            ai.inode_grace_expires = None
+        else:
+            ai.inode_grace_expires = \
+                datetime.now().replace(microsecond=0) + timedelta(seconds=int(inode_grace))
+
+        return ai
+
+    def update(self):
+        self.db_cursor.execute(
+            """UPDATE entry
+                 SET blocks_used = ?,
+                     block_soft_limit = ?,
+                     block_hard_limit = ?,
+                     inodes_used = ?,
+                     inode_soft_limit = ?,
+                     inode_hard_limit = ?,
+                     block_grace_expires = ?,
+                     inode_grace_expires = ?,
+                     block_quota_notify = ?,
+                     block_hard_notify = ?,
+                     block_ok_notify = ?,
+                     inode_quota_notify = ?,
+                     inode_hard_notify = ?,
+                     inode_ok_notify = ?,
+                     last_update = ?,
+                     username = ?
+                 WHERE filesystem = ?
+                   AND uid = ?""",
+            (self.blocks_used, self.block_quota, self.block_limit,
+             self.inodes_used, self.inode_quota, self.inode_limit,
+             self.block_grace_expires and self.block_grace_expires.isoformat(),
+             self.inode_grace_expires and self.inode_grace_expires.isoformat(),
+             self.block_quota_notify and self.block_quota_notify.isoformat(),
+             self.block_hard_notify and self.block_hard_notify.isoformat(),
+             self.block_ok_notify and self.block_ok_notify.isoformat(),
+             self.inode_quota_notify and self.inode_quota_notify.isoformat(),
+             self.inode_hard_notify and self.inode_hard_notify.isoformat(),
+             self.inode_ok_notify and self.inode_ok_notify.isoformat(),
+             datetime.now().isoformat(),
+             self.username, self.filesystem, self.uid))
+        if self.db_cursor.rowcount == 0:
+            self.db_cursor.execute(
+                """INSERT INTO entry
+                     (blocks_used, block_soft_limit, block_hard_limit,
+                      inodes_used, inode_soft_limit, inode_hard_limit,
+                      block_grace_expires, inode_grace_expires,
+                      block_quota_notify, block_hard_notify, block_ok_notify,
+                      inode_quota_notify, inode_hard_notify, inode_ok_notify,
+                      last_update, username, filesystem, uid)
+                     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+                (self.blocks_used, self.block_quota, self.block_limit,
+                 self.inodes_used, self.inode_quota, self.inode_limit,
+                 self.block_grace_expires and self.block_grace_expires.isoformat(),
+                 self.inode_grace_expires and self.inode_grace_expires.isoformat(),
+                 self.block_quota_notify and self.block_quota_notify.isoformat(),
+                 self.block_hard_notify and self.block_hard_notify.isoformat(),
+                 self.block_ok_notify and self.block_ok_notify.isoformat(),
+                 self.inode_quota_notify and self.inode_quota_notify.isoformat(),
+                 self.inode_hard_notify and self.inode_hard_notify.isoformat(),
+                 self.inode_ok_notify and self.inode_ok_notify.isoformat(),
+                 datetime.now().isoformat(),
+                 self.username, self.filesystem, self.uid))
--- update_cache.py
+++ update_cache.py
@@ -2,13 +2,13 @@
 
 import datetime
 import pwd
-import subprocess
 import sys
 
 from datetime import datetime, timedelta
 
 from pysqlite2 import dbapi2 as sqlite  # https://github.com/ghaering/pysqlite
 
+from model import *
 
 cache = sqlite.connect('cache')
 cur = cache.cursor()
@@ -27,54 +27,7 @@ finally:
 # Update quotas for every account on the system.
 for pwd_entry in pwd.getpwall():
     for fs in filesystems:
-        qt = subprocess.Popen(['quotatool', '-u', str(pwd_entry.pw_uid), '-d', fs], stdout=subprocess.PIPE)
-        stdout, stderr = qt.communicate()
-        uid, qt_fs, \
-            blocks_used, block_quota, block_limit, block_grace, \
-            inodes_used, inode_quota, inode_limit, inode_grace = \
-            stdout.split()
-        block_grace = int(block_grace)
-        inode_grace = int(inode_grace)
-
-        if block_grace == 0:
-            block_grace_expires = None
-        else:
-            block_grace_expires = \
-                (datetime.now().replace(microsecond=0) + timedelta(seconds=block_grace)).isoformat()
-        if inode_grace == 0:
-            inode_grace_expires = None
-        else:
-            inode_grace_expires = \
-                (datetime.now().replace(microsecond=0) + timedelta(seconds=inode_grace)).isoformat()
-        cur.execute("""UPDATE entry
-                         SET blocks_used = ?,
-                             block_soft_limit = ?,
-                             block_hard_limit = ?,
-                             inodes_used = ?,
-                             inode_soft_limit = ?,
-                             inode_hard_limit = ?,
-                             block_grace_expires = ?,
-                             inode_grace_expires = ?,
-                             last_update = ?,
-                             username = ?
-                         WHERE filesystem = ?
-                           AND uid = ?""",
-                    (blocks_used, block_quota, block_limit,
-                     inodes_used, inode_quota, inode_limit,
-                     block_grace_expires, inode_grace_expires,
-                     datetime.now().isoformat(),
-                     pwd_entry.pw_name, fs, pwd_entry.pw_uid))
-        if cur.rowcount == 0:
-            cur.execute("""INSERT INTO entry
-                             (blocks_used, block_soft_limit, block_hard_limit,
-                              inodes_used, inode_soft_limit, inode_hard_limit,
-                              block_grace_expires, inode_grace_expires,
-                              last_update, username, filesystem, uid)
-                             VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
-                    (blocks_used, block_quota, block_limit,
-                     inodes_used, inode_quota, inode_limit,
-                     block_grace_expires, inode_grace_expires,
-                     datetime.now().isoformat(),
-                     pwd_entry.pw_name, fs, pwd_entry.pw_uid))
+        ai = AccountInfo.from_quotatool(fs, pwd_entry.pw_uid, cur)
+        ai.update()
 
 cache.commit()
