2e0acda951450eefba53baf8d1a142c987f63719
Phil Gold <phil@cs.jhu.edu>
2015-06-11 10:15:42 -0400

    Move email templates into config file and dedicated template files.

--- send_emails.py
+++ send_emails.py
@@ -2,17 +2,20 @@
 
 from config import *
 from model import *
-from templates import *
 
 import email.MIMEText
 import smtplib
 import textwrap
+
+import jinja2  # http://jinja.pocoo.org
 
 from pysqlite2 import dbapi2 as sqlite  # https://github.com/ghaering/pysqlite
 
 config = load_config_file()
 cache = sqlite.connect(config['cache'])
 cur = cache.cursor()
+
+jj_env = jinja2.Environment(loader=jinja2.FileSystemLoader('/'))
 
 def send_email(to, subject, body):
     to_addr = '%s@%s' % (to, config['domain'])
@@ -30,56 +33,58 @@ def send_email(to, subject, body):
     s.sendmail(from_addr, to_addr, msg.as_string())
     s.quit()
 
-def pick_template(notification_state, current_state, grace_expires, quota_notify, hard_notify, ok_notify):
-    if current_state > notification_state:
-        # Things have gotten worse; we should send an email.
-        if (notification_state, current_state) == (QuotaState.hard_limit, QuotaState.grace_expired):
-            # But we don't send emails if they've already hit their hard limit.
-            return None
-        if current_state == QuotaState.soft_limit:
-            return TemplateOverQuota
-        if current_state == QuotaState.hard_limit:
-            return TemplateHardLimit
-        if current_state == QuotaState.grace_expired:
-            return TemplateGraceExpired
-    if current_state == QuotaState.under_quota and \
-            notification_state != QuotaState.under_quota and \
-            (datetime.now() - quota_notify) > config['notification_hysteresis']:
-        return TemplateUnderQuota
-    return None
+def send_email_p(changes, last_notification):
+    # If any state has gotten worse, send an email.
+    for new_state, old_state, area in changes:
+        if new_state > old_state and (old_state, new_state) != (QuotaState.hard_limit, QuotaState.grace_expired):
+            return True
+    # No state is worse than it was.
+    # Only send an email if all states are under quota...
+    for new_state, old_state, area in changes:
+        if new_state != QuotaState.under_quota:
+            return False
+    # ...and at least one old state was over quota somehow.
+    for new_state, old_state, area in changes:
+        if old_state != QuotaState.under_quota:
+            # Only send email if notification hysteresis has passed.
+            return (datetime.now() - last_notification) > timedelta(minutes=config['notification_hysteresis'])
+    # At this point, we've covered all of the circumstances under which we'd
+    # want to send an email, so the default is not to send one.
+    return False
 
 def handle_state_change(ai):
-    if ai.current_block_state == ai.notification_block_state and \
-            ai.current_inode_state == ai.notification_inode_state:
-        return
-    block_template = pick_template(ai.notification_block_state,
-                                   ai.current_block_state,
-                                   ai.block_grace_expires,
-                                   ai.block_quota_notify,
-                                   ai.block_hard_notify,
-                                   ai.block_ok_notify)
-    inode_template = pick_template(ai.notification_inode_state,
-                                   ai.current_inode_state,
-                                   ai.inode_grace_expires,
-                                   ai.inode_quota_notify,
-                                   ai.inode_hard_notify,
-                                   ai.inode_ok_notify)
-    if ai.current_inode_state > ai.current_block_state and inode_template:
-        general_template = inode_template
-    else:
-        general_template = block_template
-    if general_template.header != '':
-        msg = general_template.header + '\n\n'
-    else:
-        msg = ''
-    if block_template:
-        msg += '\n'.join(textwrap.wrap(block_template.block_text.render(ai=ai)))
-        msg += '\n\n'
-    if inode_template:
-        msg += '\n'.join(textwrap.wrap(inode_template.inode_text.render(ai=ai)))
-        msg += '\n\n'
-    msg += general_template.footer
-    send_email('phil', general_template.subject.render(ai=ai), msg)
+    changes = [(ai.current_block_state, ai.notification_block_state, 'block'),
+               (ai.current_inode_state, ai.notification_inode_state, 'inode')]
+    # See if the person is currently under quota.
+    under_quota = True
+    for new_state, old_state, area in changes:
+        if new_state != QuotaState.under_quota:
+            under_quota = False
+    # If they're not under quota, we only care about the areas where they're
+    # over.
+    if not under_quota:
+        changes = [c for c in changes if c[0] != QuotaState.under_quota]
+    changes.sort()
+    changes.reverse()
+    if send_email_p(changes, ai.last_notification):
+        summary = ''
+        details = []
+        for new_state, old_state, area in changes:
+            if old_state == new_state:
+                template_key = '%s_summary_old' % area
+            else:
+                template_key = '%s_summary_new' % area
+            template_str = config['templates'][new_state.key][template_key]
+            if template_str:
+                sum_text = jinja2.Template(config['templates'][new_state.key][template_key]).render(ai=ai)
+                if summary == '':
+                    summary = sum_text
+                else:
+                    summary += '  Also, %s%s' % (sum_text[0].lower(), sum_text[1:])
+            details.append(jinja2.Template(config['templates'][new_state.key]['%s_detail' % area]).render(ai=ai))
+        worst_state = changes[0][0]
+        message = jj_env.get_template(config['templates'][worst_state.key]['main_file']).render(ai=ai, summary=summary, details=details)
+        send_email('phil', jinja2.Template(config['templates'][worst_state.key]['subject']).render(ai=ai), message)
 
 for ai in AccountInfo.all(cur):
     handle_state_change(ai)
